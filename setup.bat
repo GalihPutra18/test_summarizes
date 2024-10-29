42e59074559e26ed0afe3344a9c4f03b8c296572
GalihPutra18 <113079774+GalihPutra18@users.noreply.github.com>
2024-10-29 20:08:20 +0700

    Delete setup.bat

--- setup.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\Users\ThinkPad i7\AppData\Local\Programs\Python\Python311\python.exe" -m pip install -r requirements.txt
-"C:\Users\ThinkPad i7\AppData\Local\Programs\Python\Python311\python.exe" -m spacy download en_core_web_sm
-
-
